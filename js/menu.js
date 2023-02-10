273e9b483b70349be653cc325c357315ec4b1036
vladyslav9311 <108949362+vladyslav9311@users.noreply.github.com>
2023-02-10 03:44:55 +0200

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.modal.classList.toggle("is-hidden-m");
+  }
+  
+})();
